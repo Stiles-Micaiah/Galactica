1c4b4a9145a9182f7b31f6201a8a38ad6a83d9e9
stiles-micaiah <syluxprime40@gmail.com>
2019-05-12 22:30:52 -0600

    01

---
+++ app/functioning-game.js
@@ -0,0 +1,46 @@
+var modifier = 10
+
+var health = 100
+
+var playerHealth = 100
+
+var winCount = 0
+
+
+document.getElementById("healthLvl").innerText = health
+
+
+function whenPressedM() {
+  health--
+  // alert(health.toLocaleString)
+  drawPage()
+}
+
+function whenPressedI() {
+  health -= 10
+  // alert(health.toLocaleString)
+  drawPage()
+}
+
+function whenPressedC() {
+  health -= 20
+  // alert(health.toLocaleString)
+  drawPage()
+}
+
+function whenPressedK() {
+  health -= 29
+  // alert(health.toLocaleString)
+  drawPage()
+}
+
+
+function drawPage() {
+
+
+
+  document.getElementById("healthLvl").innerText = health
+
+  document.getElementsByClassName("progress-bar")[0].setAttribute("style", `width: ${health}%`)
+
+}
--- app/game.js
+++ app/game.js
@@ -1,56 +1,56 @@
-// let modifier = 10
+let modifier = 10
 
 let health = 1000
 
-// let playerHealth = 100
+let playerHealth = 100
 
-// let winCount = 0
+let winCount = 0
 
 
-// setInterval(enemyAttack, 3000);
+setInterval(enemyAttack, 3000);
 
 drawPage()
 
-// function enemyAttack() {
-//   playerHealth -= 1.5 * modifier
+function enemyAttack() {
+  playerHealth -= 1.5 * modifier
 
 
-// }
+}
 
 
 
 function whenPressedM() {
   health -= 100
-  // alert(health.toLocaleString)
+  alert(health.toLocaleString)
   drawPage()
 }
 
 function whenPressedI() {
 
   health -= 150
-  // alert(health.toLocaleString)
+  alert(health.toLocaleString)
   drawPage()
 }
 
 function whenPressedC() {
 
   health -= 230
-  // alert(health.toLocaleString)
+  alert(health.toLocaleString)
   drawPage()
 }
 
 function whenPressedK() {
 
   health -= 370
-  // alert(health.toLocaleString)
+  alert(health.toLocaleString)
   drawPage()
 }
 
 
 function drawPage() {
-  // health < 0 ? health = 0 : health += 0
-  // playerHealth < 0 ? playerHealth = 0 : playerHealth += 0
-
+  health < 0 ? health = 0 : health += 0
+  playerHealth < 0 ? playerHealth = 0 : playerHealth += 0
+  debugger
   if (health <= 0) {
     health = 0
     document.getElementsByClassName("weapon").disabled = true
@@ -64,15 +64,15 @@ function drawPage() {
 
   document.getElementsByClassName("progress-bar")[0].setAttribute("style", `width: ${health / 10}%`)
 
-  // if (modifier > 10) {
-  //   document.getElementById("weapon02").disabled = false
-  // } else if (modifier > 20) {
-  //   document.getElementById("weapon03").disabled = false
-  // } else if (modifier > 30) {
-  //   document.getElementById("weapon04").disabled = false
-  // }
+  if (modifier > 10) {
+    document.getElementById("weapon02").disabled = false
+  } else if (modifier > 20) {
+    document.getElementById("weapon03").disabled = false
+  } else if (modifier > 30) {
+    document.getElementById("weapon04").disabled = false
+  }
 
-  // health < 1 ? enemyDied() : playerHealth < 1 ? playerDied() : health += 0
+  health < 1 ? enemyDied() : playerHealth < 1 ? playerDied() : health += 0
 }
 function reset() {
   // modifier = 10
@@ -81,26 +81,26 @@ function reset() {
 
 }
 
-// function playerDied() {
-//   document.getElementsByClassName("weapons").disabled = true;
-//   setTimeout(playerDiedS, 1000)
-// }
-// function playerDiedS() {
-//   alert("Lol u ded");
-//   reset()
-// }
-// function enemyDied() {
-//   modifier += 10
-//   winCount++
-//   if (winCount = 4) {
+function playerDied() {
+  document.getElementsByClassName("weapons").disabled = true;
+  setTimeout(playerDiedS, 1000)
+}
+function playerDiedS() {
+  alert("Lol u ded");
+  reset()
+}
+function enemyDied() {
+  modifier += 10
+  winCount++
+  if (winCount = 4) {
 
-//   }
-//   document.getElementById("enemyShip").setAttribute("src", "")
-//   setTimeout(enemyDiedS, 2000)
-// }
-// function enemyDiedS() {
-//   prompt("Enemy rinforcements inbound")
-//   document.getElementById("enemyShip").setAttribute("src", "enemy" + winCount + ".png")
-//   health = 100 * modifier
+  }
+  document.getElementById("enemyShip").setAttribute("src", "")
+  setTimeout(enemyDiedS, 2000)
+}
+function enemyDiedS() {
+  prompt("Enemy rinforcements inbound")
+  document.getElementById("enemyShip").setAttribute("src", "enemy" + winCount + ".png")
+  health = 100 * modifier
 
-// }
+}
